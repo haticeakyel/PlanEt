e61f816f01f5271573c37c3a9ec7a539cc40341e
Hatice Akyel <haticeeakyel@gmail.com>
2023-05-13 22:40:20 +0300

    [feature] add login part

--- front-end/src/common/RouterPage.js
+++ front-end/src/common/RouterPage.js
@@ -2,12 +2,15 @@ import { BrowserRouter, Route, Routes} from "react-router-dom";
 import React from 'react'
 import Main from "../components/Main";
 import SignUp from "../components/SignUp";
+import Login from "../components/Login";
+
 const RouterPage = () => {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" Component={Main}/>
       <Route path="/register" Component={SignUp}/>
+      <Route path="/login" Component={Login}/>
       </Routes>
           
     </BrowserRouter>
